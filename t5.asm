6682ab4fd67e05a04c8f61cf7e2bf721d906ca9c
CesarDarinel <cesardarinelortiz@gmail.com>
2018-10-11 13:39:38 -0400

    Update t5.asm

--- t5.asm
+++ t5.asm
@@ -1,112 +1,113 @@
 ; Autor: Cesar Darinel Ortiz
 ; Tarea: 3 laboratorio
 ; Fecha Entrega: 11/10/2018
-
 .model small
 .stack 256
 .data
 ; ========================Variables declaradas aquí===============
-letrero DB 'INSERTE un numero maximo 4 digitos -->: $'
-text2 DB 'No es par : $'              ; Cadena a desplegar
-text DB 'Es par : $'              ; Cadena a desplegar
-text3 DB 'No es Primo : $'              ; Cadena a desplegar
+letrero DB 'INSERTE un numero maximo 4 digitos -->: $'; Cadena a desplegar
+text2 DB 'No es par : $'             ; Cadena a desplegar
+text DB 'Es par : $'                 ; Cadena a desplegar
+text3 DB 'No es Primo : $'           ; Cadena a desplegar
 text4 DB 'Es Primo : $'              ; Cadena a desplegar
-count dw ?            ; buffer de lectura de cadena
-num dw ?
-a dw 1000
-b db 100
-c db 10
+count dw ?                           ; arreglo de lectura de cadena
+primov dw ?                           ; arreglo de lectura de cadena
+num dw ?                             ; arreglo de lectura de cadena
+a dw 1000                            ; valor para conseguir los miles
+b db 100                             ; valor para conseguir los centenas
+c db 10                              ; valor para conseguir los decimales
 .code
 ; ========================================================
 inicio:
-           mov ax,@data
-           mov ds,ax
-           mov es,ax                    ; set segment register
-           and sp,not 3                 ; align stack to avoid AC fault
+        mov ax,@data
+        mov ds,ax
+        mov es,ax                    ; set segment register
+        and sp,not 3                 ; align stack to avoid AC fault
 ; ==================Código==================================
-           
 reinicio:
-           mov ah,09                    ; Para mostrar en pantalla una cadena
-           mov dx, offset letrero       ; posición de la cadena a montar
-           int 21h                      ; llamó al sistema
-           mov bx,00                    ; inicializar en cero
-
+        mov ah,09                    ; Para mostrar en pantalla una cadena
+        mov dx, offset letrero       ; posición de la cadena a montar
+        int 21h                      ; llamó al sistema
+        mov bx,00                    ; inicializar en cero
 ciclodelectura:
-           mov AH,1
-           INT 21H  
-           SUB AL,48
-           MUL a                     
-           mov num,AX
+        mov AH,1                     ;  para lectura de teclado.
+        int 21H                      ;  llamada al SO
+        sub AL,48                    ;  resto 48 para tenerlo en ASCII
+        mul a                        ;  multiplico para agregar los ceros
+        mov num,AX                   ;  muevo el valor a la variable num
             
-            mov AH,1
-            INT 21H  
-            SUB AL,48
-            MUL b                       
-            add num,AX
+        mov AH,1                     ;  para lectura de teclado.
+        int 21H                      ;  llamada al SO
+        sub AL,48                    ;  resto 48 para tenerlo en ASCII
+        mul b                        ;  multiplico para agregar los ceros
+        mov num,AX                   ;  muevo el valor a la variable num
             
-            mov AH,1
-            INT 21H  
-            SUB AL,30H
-            MUL c                     
-            ADD num,AX
-            
-            mov AH,1
-            INT 21H                     
-            SUB AL,30H
-            ADD num,AX
-
-
-es_par:               
-           mov dx, 0h
-           mov ax, num
-           mov bx, 2
-           div bx
-           cmp dx,0h                    ; verificar si se pulsa el Enter.
-           jne nopar
-           mov ah,09                    ; Para mostrar en pantalla una cadena
-           mov dx, offset text        ; posición de la cadena a montar
-           int 21h
-           jmp init                      ; Llamada al sistema        
- nopar:        
+        mov AH,1                     ;  para lectura de teclado.
+        int 21H                      ;  llamada al SO
+        sub AL,48                    ;  resto 48 para tenerlo en ASCII
+        mul c                        ;  multiplico para agregar los ceros
+        mov num,AX                   ;  muevo el valor a la variable num
+        
+        mov AH,1                     ;  para lectura de teclado.
+        int 21H                      ;  llamada al SO
+        sub AL,48                    ;  resto 48 para tenerlo en ASCII
+        mov num,AX                   ;  muevo el valor a la variable num
+es_par:
+        mov count,1                  ; seteo la variable en 1   
+        mov primov,0h                 ; seteo la variable en 1               
+        call init                    ; seteo en cero 
+        mov ax, num                  ; muevo el valor numerando 
+        mov bx, 2                    ; muevo el divisor
+        div bx                       ; realizo la division 
+        cmp dx,0h                    ; comparo el residuo con cero
+        jne nopar                    ; si no es cero salto a nopar
         mov ah,09                    ; Para mostrar en pantalla una cadena
-           mov dx, offset text2        ; posición de la cadena a montar
-           int 21h                      ; Llamada al sistema       
+        mov dx, offset text          ; posición de la cadena a montar
+        int 21h                      ; llama al SO
+        jmp primo                    ; salto a buscar el numero primo      
+nopar:        
+        mov ah,09                    ; Para mostrar en pantalla una cadena
+        mov dx, offset text2         ; posición de la cadena a montar
+        int 21h                      ; Llamada al sistema       
 init:
-    mov count,1
-    mov cl,0
-    mov dx,0
- primo:
-    mov ax, count
-    mov bx,num
-    cmp ax, bx
-    je print
-    mov ax, num
-    mov bx, count
-    div bx
-    cmp dx,0h   
-    jne continuociclo
-    inc cl  
+        mov dx, 0h                   ;inicializo en cero 
+        mov ax, 0h                   ;inicializo en cero 
+        mov bx, 0h                   ;inicializo en cero
+        mov cl, 0h                   ;inicializo en cero 
+        ret
+primo:
+        call init                    ; seteo en cero 
+        mov ax, count                ;
+        mov bx, num                  ;
+        cmp ax, bx                   ;
+        je print                     ;
+        call init                    ; seteo en cero 
+        mov ax, num                  ;
+        mov bx, count                ;
+        div bx                       ;
+        cmp dx,0h                    ;
+        jne continuociclo            ;
+        inc primov                   ;
 continuociclo:   
-    inc count
-    jmp primo
-
+        inc count                    ;
+        jmp primo                    ;
+        
 print:
-    cmp cl,50d
-    jne noprimo
-        mov ah,09                    ; Para mostrar en pantalla una cadena
+        call init    
+        mov ax, primov
+        cmp ax,2                    ;
+        jne noprimo                 ;
+        mov ah,09                   ; Para mostrar en pantalla una cadena
         mov dx, offset text4        ; posición de la cadena a montar
-        int 21h
-        jmp Salida                      ; Llamada al sistema        
+        int 21h                     ;
+        jmp Salida                  ; Llamada al sistema        
  noprimo:        
-        mov ah,09                    ; Para mostrar en pantalla una cadena
+        mov ah,09                   ; Para mostrar en pantalla una cadena
         mov dx, offset text3        ; posición de la cadena a montar
-        int 21h                      ; Llamada al sistema  
-
+        int 21h                     ; Llamada al sistema  
 ; ********************************************************************
 Salida:
     
-       
-
 ; ========================================================
 .exit
 end inicio
