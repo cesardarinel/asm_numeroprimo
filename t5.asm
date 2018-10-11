4ca2a6d0509f5df7fd854c42ea6fa390c653c795
cesar darinel ortiz <cesardarinelortiz@gmail.com>
2018-10-11 19:16:50 -0400

    cambios en el ciclo

--- t5.asm
+++ t5.asm
@@ -12,8 +12,6 @@ text3 DB ' No es Primo $'           ; Cadena a desplegar
 text4 DB ' Es Primo $'              ; Cadena a desplegar
 ceros DB ' El numero 0 es un numero neutro y no está considerado como numero primo. $'
 unos DB ' El numero 1 no está considerado como numero primo. $'
-count dw ?                           ; arreglo de lectura de cadena
-primov dw ?                          ; arreglo de lectura de cadena
 num dw ?                             ; arreglo de lectura de cadena
 a dw 1000                            ; valor para conseguir los miles
 b db 100                             ; valor para conseguir los centenas
@@ -50,10 +48,9 @@ ciclodelectura:
         mov AH,1                     ;  para lectura de teclado.
         int 21H                      ;  llamada al SO
         mov num,AX                   ;  muevo el valor a la variable num
-        add num,48
 es_par:
-        mov count,1                  ; iniciar la variable en 1   
-        mov primov,0h                ; iniciar la variable en 1               
+        mov di,1                  ; iniciar la variable en 1   
+        mov si,0                ; iniciar la variable en 1               
         call init                    ; iniciar en cero 
         mov ax, num                  ; muevo el valor numerando 
         mov bx, 2                    ; muevo el divisor
@@ -85,9 +82,9 @@ init:
 valida:
         call init                    ; limpio datos
         mov ax, num                  ; muevo los valores digitados  
-        cmp al,0                    ; comparo con cero
+        cmp al,0h                    ; comparo con cero
         je es_cero                   ; pantalla cuando es cero 
-        cmp al,1                    ; comparo con uno
+        cmp al,1h                    ; comparo con uno
         je es_uno                    ; pantalla cunado es uno 
         jmp primo                    ; salto a primo 
 es_cero:
@@ -112,29 +109,30 @@ es_uno:
 
 primo:
         call init                    ; limpio datos
-        mov ax, count                ; muevo el contador a AX
+        mov ax, di                   ; muevo el contador a AX
         mov bx, num                  ; muevo el numero a bx
         cmp ax, bx                   ; comparo los números
         je print                     ; son iguales salgo del ciclo
         call init                    ; limpio datos
         mov ax, num                  ; muevo el valor a ax
-        mov bx, count                ; muevo el contador a bx
+        mov bx, di                ; muevo el contador a bx
         div bx                       ; divido 
         cmp dx,0h                    ; comparo el resultado  
         jne continuociclo            ; si no son iguales continuo
-        inc primov                   ; si son iguales aumento
+        inc si                   ; si son iguales aumento
 continuociclo:                            
-        inc count                    ; incremento contador 
+        inc di                    ; incremento contador 
         jmp primo                    ; continuo ciclo
         
 print:
         call init                    ; limpió datos
-        mov ax, primov               ; muevo el valor a ax
-        cmp ax,2h                    ; comparo el valor con 2
-        jne noprimo                  ; si no es igual 
+        mov bx, si               ; muevo el valor a ax
+        cmp bl,2h                    ; comparo el valor con 2
+        jge noprimo                  ; si no es igual 
         mov ah,09                    ; Para mostrar en pantalla una cadena
         mov dx, offset text4         ; posición de la cadena a montar
         int 21h                      ; llamada al SO
+
         mov ah,02                    ; para mostrar carácter
         mov dl,10                    ; imprimió el carácter de salto de línea
         int 21h                      ; llamada sistema
